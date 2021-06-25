13691c49bc09014ed9def8fbba6e1ee79c272097
Ljubo Nikolic <ljubonikolic@gmail.com>
2021-06-25 21:34:04 +0200

    Brew formula update for armada version v0.3.95

--- Formula/armadactl.rb
+++ Formula/armadactl.rb
@@ -5,28 +5,28 @@
 class Armadactl < Formula
   desc "Example binary distribution using homebrew"
   homepage "https://github.com/ljubon/homebrew-armadactl"
-  version "0.3.94"
+  version "0.3.95"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/ljubon/armada/releases/download/v0.3.94/armadactl_0.3.94_darwin_amd64.tar.gz"
-      sha256 "55198b582e937ca29853d8be263c17b8f55f08b85519befa9e42568c2e35bf57"
+      url "https://github.com/ljubon/armada/releases/download/v0.3.95/armadactl_0.3.95_darwin_amd64.tar.gz"
+      sha256 "f0f64f9e2ae003ed934a2f7ac3db6abf6e24443e613a6ca75bc59492dc6472de"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/ljubon/armada/releases/download/v0.3.94/armadactl_0.3.94_darwin_arm64.tar.gz"
-      sha256 "bb5a609e4a5db2f19aee326f99e44c80f49065167f23c9ad9369ca83527e87da"
+      url "https://github.com/ljubon/armada/releases/download/v0.3.95/armadactl_0.3.95_darwin_arm64.tar.gz"
+      sha256 "1613e6085de990f54845ceb3f7f06e2dd34e2893d574c54baff5527bddbcb8d7"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ljubon/armada/releases/download/v0.3.94/armadactl_0.3.94_linux_amd64.tar.gz"
-      sha256 "5dd78436535da646041b851e76020f17698e235a760c3e994119b8727fb83238"
+      url "https://github.com/ljubon/armada/releases/download/v0.3.95/armadactl_0.3.95_linux_amd64.tar.gz"
+      sha256 "81fe10addd13a9bdcf3ab0c3bf74e003851242a380a73a8c4e74b4a4375d9ee1"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ljubon/armada/releases/download/v0.3.94/armadactl_0.3.94_linux_arm64.tar.gz"
-      sha256 "cb9a5734a75774f06521d0ef70da5ecdfa83db4b8d1edf9f1c9cdc38398a31bd"
+      url "https://github.com/ljubon/armada/releases/download/v0.3.95/armadactl_0.3.95_linux_arm64.tar.gz"
+      sha256 "2bc95ed0730fe74eb31bb9430c15f2173b7e7a6490d49d9d7a6b2329b9b4e388"
     end
   end
 
